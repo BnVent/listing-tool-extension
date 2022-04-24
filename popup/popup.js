3e1b1b23e49b3599a411d04a97ce9d2a0f626808
BnVent <brandonventurino@gmail.com>
2022-04-24 12:39:00 -0300

    Feat: list-creation section added

--- popup/popup.js
+++ popup/popup.js
@@ -10,15 +10,41 @@ const contentAddingSection = document.getElementById('content-adding-section');
 const toggleButtonContainer = document.getElementById('toggle-button-container')
 const toggleButton = document.getElementById('toggle-button')
 
+const listCreationInput = document.getElementById('list-creation-input');
+const listCreationLabel = document.getElementById('list-creation-label')
+
 var currentSection = mainSection
 
 function showSection(section) {
     currentSection.style.display = 'none'
     section.style.display = 'block'
-    currentSection = section
+    currentSection = section;
 }
 
-addListButton.onclick = () => showSection(listAddingSection)
+addListButton.onclick = () => {
+
+    showSection(listAddingSection)
+
+    const colorOptions = Object.values(document.getElementsByClassName('color-option'));
+    const defaultOption = colorOptions.splice(0, 1)[0]
+
+    const setCurrentColor = (borderColor, textColor = borderColor, labelColor = borderColor) => {
+        listCreationInput.style.borderColor = borderColor
+        listCreationInput.style.color = textColor
+        listCreationLabel.style.color = labelColor
+    }
+
+    defaultOption.setAttribute('style', `background: linear-gradient(#DBDBDB, #FFF)`);
+    defaultOption.onclick = () => {setCurrentColor('#DBDBDB', '#000', 'black')}
+
+    colorOptions.forEach(element => {
+
+        let elementColor = element.getAttribute('value');
+
+        element.setAttribute('style', `background: linear-gradient(${elementColor}, #FFF)`)
+        element.onclick = () => {setCurrentColor(elementColor)}
+    })
+}
 addContentButton.onclick = () => showSection(contentAddingSection)
 
 function addOverlay() {
